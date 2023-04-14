9d41401781242c89e56bd91731cb18e1db3ef623
MitaAlba <joselyne.cruz@alumnos.udg.mx>
2023-04-14 00:19:54 -0600

    Version 1.1.0

---
+++ database/seeders/CategorieSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class CategorieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //ACTIVAR PARA HACER SEDD o FACTORY
+        // DB::table('categories')->insert([
+        //     'name'=>'Bakery',
+        //     'description'=>'Varieties of freshly baked bread'
+
+        // ]);
+    }
+}
